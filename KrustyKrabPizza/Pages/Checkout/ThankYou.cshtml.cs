79c491d35054530a59896d5757440b09555dafa2
Paulson Hanel <Paulson.Hanel@mymhc.ca>
2022-10-01 19:22:12 -0600

    Added some links and styles to main index page. have a couple headers, added a btn to order (just redirect)

---
+++ KrustyKrabPizza/Pages/Checkout/ThankYou.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KrustyKrabPizza.Pages.Checkout
+{
+    public class ThankYouModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
