190426742abe3a7b02b861f6e518472a961f39b6
Paulson Hanel <Paulson.Hanel@mymhc.ca>
2022-10-01 20:52:54 -0600

    Added model for pizza, added code to view and model for custom pizza. Learned there is a view and he called thing behind another model. code seems to be accessable via both, from one to another. We can access the pizza because we created a class in the model for custom pizza that was a data binding to our pizzas model where we have all the properties for the pizzas. Also created a form input and buton for getting your pizza in the custom pizza view.

---
+++ KrustyKrabPizza/Models/PizzasModel.cs
@@ -0,0 +1,17 @@
+﻿namespace KrustyKrabPizza.Models
+{
+    public class PizzasModel
+    {
+        public string ImageTitle { get; set; }
+        public string PizzaName { get; set; }
+        public float BasePrice { get; set; } = 15.99f;
+        public bool HasTomatoSause { get; set; }
+        public bool HasCheese { get; set; }
+        public bool HasMushrooms { get; set; }
+        public bool HasPineapple { get; set; } // Should make label do something funny like be red or whatever idk
+        public bool HasTuna { get; set; }
+        public bool HasHam { get; set; }
+        public bool HasBeef { get; set; }
+        public float FinalPrice { get; set; }
+    }
+}
--- KrustyKrabPizza/Pages/Forms/CustomPizza.cshtml.cs
+++ KrustyKrabPizza/Pages/Forms/CustomPizza.cshtml.cs
@@ -1,3 +1,4 @@
+using KrustyKrabPizza.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,6 +6,8 @@ namespace KrustyKrabPizza.Pages.Forms
 {
     public class CustomPizzaModel : PageModel
     {
+        [BindProperty]  // Binding custom pizza (below) to the pizza model (above). Allows creation of custom pizzas from the custom pizzas view
+        public PizzasModel Pizza { get; set; }
         public void OnGet()
         {
         }
